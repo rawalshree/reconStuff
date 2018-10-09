f122bdd94c88d79c2cee01c403eb4533f728cd0b
rawalshree <rawalshreepal000@gmail.com>
2018-10-09 12:44:00 -0700

    nmap scanning for recon

--- nmap.py
+++ nmap.py
@@ -7,23 +7,23 @@ import threading
 ####### Nmap Function To add List of Active Hosts on the Network #######
 def scan(ip, subnet):
     output = subprocess.check_output("sudo nmap -n -sn " + str(ip) + "/" + str(subnet) +" -oG - | awk '/Up$/{print $2}'", shell=True).strip().decode('utf-8')
-    with open("ipList.txt", 'w+') as file:
+    with open("scanFiles/ipList.txt", 'w+') as file:
         file.write(output)
 
 
 ####### Nmap Function To add Detailed Scan #######
 def nmapDetailedScan():
-    with open("ipList.txt", "r") as file:
+    with open("scanFiles/ipList.txt", "r") as file:
         for line in file:
             output = subprocess.check_output("sudo nmap -v -sV -O -sS -T5 " + str(line), shell=True).strip().decode('utf-8')
-            with open("nmapDetailScan.txt", "a") as file:
+            with open("scanFiles/nmapDetailScan.txt", "a") as file:
                 file.write(line.rstrip('\n') + "\n" + output[output.find('\n')+1:output.rfind('\n') - 1])
                 file.write("\n====================================================================================================== \n\n\n\n")
 
 
 ####### Nikto Function To add Port 80 and 443 Scan #######
 def niktoScan():            
-    with open("ipList.txt", "r") as file:
+    with open("scanFiles/ipList.txt", "r") as file:
         for line in file:
             output = subprocess.check_output("nikto -h " + str(line) + " -p 80,443 ", shell=True).strip().decode('utf-8')
             with open("nikto.txt", "a") as file:
@@ -33,11 +33,11 @@ def niktoScan():
 
 ####### Nmap Function To check default Credentials #######
 def nmapCredScan():
-    with open("ipList.txt", "r") as file:
+    with open("scanFiles/ipList.txt", "r") as file:
         for line in file:
             output = subprocess.check_output("nmap --script http-default-accounts --script-args http-default-accounts.fingerprintfile=~/Recon_Stuff/http-default-accounts-fingerprints-nndefaccts.lua \
                                             -p 80  " + str(line), shell=True).strip().decode('utf-8')
-            with open("credentials.txt", "a") as file:
+            with open("scanFiles/credentials.txt", "a") as file:
                 file.write(line.rstrip('\n') + "\n" + output[output.find('\n')+1:output.rfind('\n') - 1])
                 file.write("\n====================================================================================================== \n\n\n\n")
 
@@ -49,16 +49,16 @@ if __name__ == "__main__":
     subnet = str(sys.argv[2])                              # Takes Subnet to Scan
 
     ####### Deletes if Nmap Verbose Scan results already exists #######
-    if os.path.exists('nmapDetailScan.txt'):
-        subprocess.call("rm nmapDetailScan.txt", shell=True)
+    if os.path.exists('scanFiles/nmapDetailScan.txt'):
+        subprocess.call("rm scanFiles/nmapDetailScan.txt", shell=True)
 
     ####### Deletes if Nikto Scan results already exists #######
-    if os.path.exists('nikto.txt'):
-        subprocess.call("rm nikto.txt", shell=True)
+    if os.path.exists('scanFiles/nikto.txt'):
+        subprocess.call("rm scanFiles/nikto.txt", shell=True)
 
     ####### Deletes if Credentials Scan results already exists #######
-    if os.path.exists('nikto.txt'):
-        subprocess.call("rm credentials.txt", shell=True)
+    if os.path.exists('scanFiles/nikto.txt'):
+        subprocess.call("rm scanFiles/credentials.txt", shell=True)
 
     ####### Downloads the file for Default Credential scan using Nmap #######
     if not os.path.exists("http-default-accounts-fingerprints-nndefaccts.lua"):
